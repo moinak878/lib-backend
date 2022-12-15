98a7fd23f53bdcff87f630bc1bd61bdfc01b985d
Moinak Banerjee <moinak878@gmail.com>
2022-12-15 10:41:25 +0530

    updating packages

--- routes/author.js
+++ routes/author.js
@@ -1,22 +1,20 @@
 // GET /authors/me: To return the details of the logged-in author.
 const express = require("express");
 const router = express.Router();
-const {
-  getUserById,
-  getUser,
-  updateUser,
-} = require("../controllers/user");
-const { isSignedIn, isAuthenticated } = require("../controllers/auth");
+// const {
+//   getUserById,
+//   getUser,
+//   updateUser,
+// } = require("../controllers/user");
+// const { isSignedIn, isAuthenticated } = require("../controllers/auth");
 
-// GET /authors: To return the response of all the authors in the database with number of books published by that author
-router.get("/authors", isSignedIn , getAllAuthors);
+// // GET /authors: To return the response of all the authors in the database with number of books published by that author
+// router.get("/authors", isSignedIn , getAllAuthors);
 
+// router.param("userId", getUserById);
+// // GET /authors/:id: To return the details of the author with the given author id with list of books.
+// router.get("/authors/:authorId", isSignedIn, getUser);
 
-router.param("userId", getUserById);
-// GET /authors/:id: To return the details of the author with the given author id with list of books.
-router.get("/authors/:authorId", isSignedIn, getUser);
-
-router.put("/authors/me", isSignedIn, isAuthenticated, updateUser);
-
+// router.put("/authors/me", isSignedIn, isAuthenticated, updateUser);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// routes
+const authRoutes = require("./routes/auth");
+// const authorRoutes = require("./routes/author");
+// const bookRoutes = require("./routes/book");
 
+app.use("/", authRoutes);
+// app.use("/", authorRoutes);
+// app.use("/", bookRoutes);
+
+// middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+// DB connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
+
+// server
 app.listen(8000, () => {
   console.log("Server is running on port 3000");
 });
